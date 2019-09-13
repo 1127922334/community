b5397afb8413a5ab4301f99ab6d988ecfec0924c
1127922334 <112722334@qq.com>
2019-09-13 16:13:57 +0800

    init  repo

---
+++ src/main/java/com/zkz/jinjiang1/Jinjiang1Application.java
@@ -0,0 +1,13 @@
+package com.zkz.jinjiang1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jinjiang1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jinjiang1Application.class, args);
+    }
+
+}
